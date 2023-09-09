e2b690774b322dce8cc19dbccdf33505de2ab8e9
luis <rendincorreal@gmail.com>
2023-09-09 16:09:26 -0500

    crecion_opciones_menu

--- momento2.py
+++ momento2.py
@@ -5,3 +5,15 @@ Motos = {
     'Royal Enfield': 'Super Meteoro 650',
 }
 
+while True:
+    print("\nMenu:")
+    print("1. Agregar motos")
+    print("2. Buscar motos")
+    print("3. Actualizar motos")
+    print("4. Eliminar motos")
+    print("5. Listar motos")
+    print("6. Salir")
+
+    opcion = input("Digite una opción: ")
+
+    
